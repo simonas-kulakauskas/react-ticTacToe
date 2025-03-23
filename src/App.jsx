27b033aa661b7ce1dd1852ebfca8b41f0439b1a7
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-23 19:43:50 +0000

    Added some styling to the grid layout, removed the individual item's classes

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ function App() {
     <>
       <h1>Tic Tac Toe</h1>
       <div className="gameBoard">
-        <div className="cell cell-1">1</div>
-        <div className="cell cell-2">2</div>
-        <div className="cell cell-3">3</div>
+        <div className="cell">1</div>
+        <div className="cell">2</div>
+        <div className="cell">3</div>
 
-        <div className="cell cell-4">4</div>
-        <div className="cell cell-5">5</div>
-        <div className="cell cell-6">6</div>
+        <div className="cell">4</div>
+        <div className="cell">5</div>
+        <div className="cell">6</div>
 
-        <div className="cell cell-7">7</div>
-        <div className="cell cell-8">8</div>
-        <div className="cell cell-9">9</div>
+        <div className="cell">7</div>
+        <div className="cell">8</div>
+        <div className="cell">9</div>
       </div>
     </>
   );
