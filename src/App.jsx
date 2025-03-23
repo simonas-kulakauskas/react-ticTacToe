87901e577348ec2e44662cecf094ca19903d86c1
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-23 19:25:12 +0000

    Created a layout with Grid.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,19 @@ function App() {
   return (
     <>
       <h1>Tic Tac Toe</h1>
+      <div className="gameBoard">
+        <div className="cell cell-1">1</div>
+        <div className="cell cell-2">2</div>
+        <div className="cell cell-3">3</div>
+
+        <div className="cell cell-4">4</div>
+        <div className="cell cell-5">5</div>
+        <div className="cell cell-6">6</div>
+
+        <div className="cell cell-7">7</div>
+        <div className="cell cell-8">8</div>
+        <div className="cell cell-9">9</div>
+      </div>
     </>
   );
 }
