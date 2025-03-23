e80359faa702bbde80057fa4024a852ad32eeb3e
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2025-03-23 21:08:33 +0000

    Currently have the logic to put down either an X or O depending on who's turn it is, it won't put one over another.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,61 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [turn, setTurn] = useState(1); // Default's to 'X' Turn (1).
+  const [board, setBoard] = useState([
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ]);
+
+  function handleClick(i, j) {
+    setBoard(
+      board.map((row, g) => {
+        if (i !== g) {
+          return row;
+        } else {
+          let newRow = row;
+          newRow[j] = turn;
+          return newRow;
+        }
+      })
+    );
+
+    turn === 1 ? setTurn(2) : setTurn(1);
+  }
+
   return (
     <>
       <h1>Tic Tac Toe</h1>
+      <h2>Turn: {turn === 1 ? "X" : "O"}</h2>
       <div className="gameBoard">
-        <div className="cell">1</div>
-        <div className="cell">2</div>
-        <div className="cell">3</div>
-
-        <div className="cell">4</div>
-        <div className="cell">5</div>
-        <div className="cell">6</div>
-
-        <div className="cell">7</div>
-        <div className="cell">8</div>
-        <div className="cell">9</div>
+        {board.map((row, i) => {
+          return row.map((cell, j) => {
+            {
+              if (cell === 0) {
+                cell = "";
+              } else if (cell === 1) {
+                cell = "X";
+              } else if (cell === 2) {
+                cell = "O";
+              }
+              return (
+                <div
+                  className="cell"
+                  key={[i, j]}
+                  onClick={() => {
+                    if (board[i][j] === 0) {
+                      handleClick(i, j);
+                    }
+                  }}
+                >
+                  {cell}
+                </div>
+              );
+            }
+          });
+        })}
       </div>
     </>
   );
